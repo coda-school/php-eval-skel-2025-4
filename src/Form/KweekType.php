2284ded6dcb4a3a59c893217616c8098a7b8b5a7
lucas <lucasmimi21110@gmail.com>
2026-01-29 19:51:31 +0100

    Modif twig

--- src/Form/KweekType.php
+++ src/Form/KweekType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType; // <-- Ajoute cette ligne
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,13 @@ class KweekType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('ContenuDuKweek')
+            ->add('ContenuDuKweek', TextareaType::class, [ // <-- Utilise TextareaType ici
+                'label' => false, // On cache le label par défaut
+                'attr' => [
+                    'rows' => 6, // Définit la hauteur par défaut
+                    'placeholder' => "What's happening?" // Placeholder déplacé ici (plus propre)
+                ]
+            ])
         ;
     }
 
